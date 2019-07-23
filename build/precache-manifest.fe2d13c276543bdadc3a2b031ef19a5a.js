2d3c0fa80f18d12c2c9d326573af2f73a4c339be
Yedi Wang <wangyedi0325@hotmail.com>
2019-07-23 16:45:26 -0700

    create github pages

---
+++ build/precache-manifest.fe2d13c276543bdadc3a2b031ef19a5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b78dee66d61ee366b2662e0d2e8df404",
+    "url": "/spotify_api_search_example/index.html"
+  },
+  {
+    "revision": "33235b78d85fa08830b0",
+    "url": "/spotify_api_search_example/static/css/main.031273fd.chunk.css"
+  },
+  {
+    "revision": "9ef0e6ae7efb40699514",
+    "url": "/spotify_api_search_example/static/js/2.b73a0406.chunk.js"
+  },
+  {
+    "revision": "33235b78d85fa08830b0",
+    "url": "/spotify_api_search_example/static/js/main.7a06e507.chunk.js"
+  },
+  {
+    "revision": "31217c900bd9edfa5fc5",
+    "url": "/spotify_api_search_example/static/js/runtime~main.3c17f4f9.js"
+  },
+  {
+    "revision": "c7cfcfbb8e808dc43cc62a4f5f80ac6b",
+    "url": "/spotify_api_search_example/static/media/album_1.c7cfcfbb.png"
+  },
+  {
+    "revision": "6f346068fc5d99f9604a4ca47babd4f3",
+    "url": "/spotify_api_search_example/static/media/pause.6f346068.png"
+  },
+  {
+    "revision": "4d509e75d40366402b6188048ad2c9d2",
+    "url": "/spotify_api_search_example/static/media/play.4d509e75.png"
+  }
+]);
