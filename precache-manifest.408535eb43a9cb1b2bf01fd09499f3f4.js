6d7292fe487f38723107b69d26ad52ca832be42b
Yedi Wang <wangyedi0325@hotmail.com>
2019-07-23 17:13:48 -0700

    Updates

--- precache-manifest.408535eb43a9cb1b2bf01fd09499f3f4.js
+++ precache-manifest.408535eb43a9cb1b2bf01fd09499f3f4.js
@@ -1,23 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ecaebf189c649b25e46774201011493c",
+    "revision": "1cbaff8ffc54a4ae648194ac3276436f",
     "url": "/spotify_api_search_example/index.html"
   },
   {
-    "revision": "b08ddb776b25176145fc",
-    "url": "/spotify_api_search_example/static/css/main.031273fd.chunk.css"
+    "revision": "f193118c162dfbd0685d",
+    "url": "/spotify_api_search_example/static/css/main.de1b5a76.chunk.css"
   },
   {
     "revision": "9ef0e6ae7efb40699514",
     "url": "/spotify_api_search_example/static/js/2.b73a0406.chunk.js"
   },
   {
-    "revision": "b08ddb776b25176145fc",
-    "url": "/spotify_api_search_example/static/js/main.0ba5dac9.chunk.js"
+    "revision": "f193118c162dfbd0685d",
+    "url": "/spotify_api_search_example/static/js/main.11df6aca.chunk.js"
   },
   {
     "revision": "31217c900bd9edfa5fc5",
     "url": "/spotify_api_search_example/static/js/runtime~main.3c17f4f9.js"
+  },
+  {
+    "revision": "f9a5c45277e6555adc7ee38e9a48a241",
+    "url": "/spotify_api_search_example/static/media/ABeeZee-Regular.f9a5c452.otf"
+  },
+  {
+    "revision": "877e8de72d7600fdddcd6c08b1af3860",
+    "url": "/spotify_api_search_example/static/media/Aganè 55 (roman).877e8de7.ttf"
   },
   {
     "revision": "c7cfcfbb8e808dc43cc62a4f5f80ac6b",
