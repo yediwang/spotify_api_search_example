db904f01044ae61336961f64d69866eed2c84bb5
Yedi Wang <wangyedi0325@hotmail.com>
2019-07-23 17:18:25 -0700

    add fonts

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/spotify_api_search_example/precache-manifest.4321eb14ec0eade2750d16e24611042e.js"
+  "/spotify_api_search_example/precache-manifest.408535eb43a9cb1b2bf01fd09499f3f4.js"
 );
 
 self.addEventListener('message', (event) => {
--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import Layout from './layouts/Layout.jsx';
+import Layout from "./layouts/Layout.jsx";
 
 import rootReducer from "./reducers";
 
 const store = createStore(rootReducer);
 
 class App extends React.Component {
-
   render() {
     return (
       <Provider store={store}>
-        <Layout/>
+        <Layout />
       </Provider>
     );
-  };
-};
+  }
+}
 
 export default App;
